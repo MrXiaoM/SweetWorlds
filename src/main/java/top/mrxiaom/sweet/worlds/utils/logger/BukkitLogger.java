fc4f405e95fad45dcf0ce71e6838260b8d7981a7
人間工作 <coolxiaom95@gmail.com>
2025-05-17 22:02:18 +0800

    enhance logger

--- src/main/java/top/mrxiaom/sweet/worlds/utils/logger/BukkitLogger.java
+++ src/main/java/top/mrxiaom/sweet/worlds/utils/logger/BukkitLogger.java
@@ -1,7 +1,9 @@
 package top.mrxiaom.sweet.worlds.utils.logger;
 
+import top.mrxiaom.pluginbase.utils.Util;
 import top.mrxiaom.sweet.worlds.SweetWorlds;
 
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -11,14 +13,31 @@ public class BukkitLogger extends AbstractLogger {
         super(name);
     }
 
+    private String handleName(String name) {
+        if (name.startsWith("top.mrxiaom.sweet.worlds.libs.")) {
+            return "[" + name.substring(30) + "]";
+        }
+        return "[" + name + "]";
+    }
+
     private void log(Level level, String name, String s) {
-        logger.log(level, "[" + name + "] " + s);
+        logger.log(level, handleName(name) + " " + s);
     }
     private void log(Level level, String name, String s, Object[] objects) {
-        logger.log(level, "[" + name + "] " + s, objects);
+        StringBuilder sb = new StringBuilder(handleName(name));
+        sb.append(" ");
+        List<String> split = Util.split(s, "{}");
+        int size = split.size();
+        for (int i = 0; i < size; i++) {
+            sb.append(split.get(i));
+            if (i < size - 1) {
+                sb.append("{").append(i).append("}");
+            }
+        }
+        logger.log(level, sb.toString(), objects);
     }
     private void log(Level level, String name, String s, Throwable throwable) {
-        logger.log(level, "[" + name + "] " + s, throwable);
+        logger.log(level, handleName(name) + " " + s, throwable);
     }
 
     @Override
